5c4cd87a2317f4c2bb91997f42b8d756b6ae34b7
Lucasmgp <lucasmgpereira18@gmail.com>
2022-05-22 14:11:17 -0300

    Atualização do desafio de HTML-CSS-JS.

--- html-css-js/Desafio/JS/index.js
+++ html-css-js/Desafio/JS/index.js
@@ -1,30 +1,79 @@
-window.onload = function(){
+window.onload = function () {
+    var usuarioCorreto = []
+    var senhaCorreta = []
+    //Pegar dados no JSON
+    let dadosUser = async function () {
+        let url = `./usuario.json`
+        let dadosFetch = await fetch(url)
+        let dadosJson = await dadosFetch.json()
+        for (let dadosUsuario of dadosJson.users) {
+            usuarioCorreto.push(dadosUsuario.user)
+            senhaCorreta.push(dadosUsuario.pws)
+        }
+        clickEntrar(usuarioCorreto, senhaCorreta)
+    }
+    dadosUser(usuarioCorreto, senhaCorreta)
+    //Pegar os elementos do HTML
     const body = document.getElementById("body")
     const user = document.getElementById("user")
     const senha = document.getElementById("senha")
-    const entrar = document.getElementById("link_panel")
+    const entrar = document.getElementById("link_painel")
     const quad_erro = document.getElementById("quadrado-erro")
     const btn_erro = document.getElementById("btn_ok")
-    entrar.addEventListener("click", function(){
-        if(user.value == "admin" && senha.value=="admin"){
-            window.open("panel.html", "_parent")
-        }else{
-            body.classList.add("cor_escuro")
-            entrar.classList.remove("link_panel")
-            user.classList.add("digitar-escuro")
-            senha.classList.add("digitar-escuro")
-            quad_erro.classList.remove("desaparecer")
-            user.setAttribute("disabled", true)
-            senha.setAttribute("disabled", true)
-        }
-    })
-    btn_erro.addEventListener("click", function(){
+    const escrita_error = document.getElementById("fala-erro")
+    //Função mudar o CSS
+    function error_caixa() {
+        body.classList.add("cor_escuro")
+        entrar.classList.remove("link_painel")
+        user.classList.add("digitar-escuro")
+        senha.classList.add("digitar-escuro")
+        quad_erro.classList.remove("desaparecer")
+        user.setAttribute("disabled", true)
+        senha.setAttribute("disabled", true)
+    }
+    //Função saida do erro retorna o CSS
+    function saida_error() {
         quad_erro.classList.add("desaparecer")
         user.removeAttribute("disabled")
         senha.removeAttribute("disabled")
         body.classList.remove("cor_escuro")
-        entrar.classList.add("link_panel")
+        entrar.classList.add("link_painel")
         user.classList.remove("digitar-escuro")
         senha.classList.remove("digitar-escuro")
+    }
+    //Função validar login e senha
+    function clickEntrar(user_entrar, senha_entrar) {
+        //paramentros do arquivo JSON
+        entrar.addEventListener("click", function () {
+            try {
+                let i = 0
+                //Cmparando o array do arquivo JSON com o que foi digitado
+                while (user_entrar[i] != user.value) {
+                    i++
+                    if (i > user_entrar.length) {
+                        i = 0
+                        while (senha_entrar[i] != senha.value) {
+                            i++
+                            if (i > senha_entrar.length) {
+                                break
+                            }
+                        }
+                        break
+                    }
+                }
+                //Tratamento de erro com Try Catch
+                if (user.value != user_entrar[i] && senha.value != senha_entrar[i]) throw new Error("Usuário e senha inválidos! Tente novamente.")
+                if (user.value != user_entrar[i]) throw new Error("Usuário inválido! Tente Novamente.")
+                if (senha.value != senha_entrar[i]) throw new Error("Senha inválida! Tente novamente.")
+                window.open("painel.html", "_parent")
+            } catch (error) {
+                error_caixa()
+                escrita_error.innerHTML = error.message
+            }
+        })
+    }
+    //Botão ok para sair do erro de login.    
+    btn_erro.addEventListener("click", function () {
+        saida_error()
     })
 }
--- html-css-js/Desafio/JS/painel.js
+++ html-css-js/Desafio/JS/painel.js
@@ -1,4 +1,6 @@
-window.onload = function(){
+import { clientes as dadosClientes } from "./clientes"
+import { produtos as dadosProdutos } from "./produtos"
+window.onload = function () {
     const botao_voltar = document.getElementById("botao_voltar")
     const botao_avancar = document.getElementById("botao_avancar")
     const botao_voltar_prod = document.getElementById("botao_voltar_prod")
@@ -10,53 +12,145 @@ window.onload = function(){
     const aba_cliente = document.getElementById("aba_cliente")
     const aba_prod = document.getElementById("aba_prod")
     const aba_ped = document.getElementById("aba_ped")
+    const valor_cod_cli = document.getElementById("codigo_cliente")
+    const valor_nome_cli = document.getElementById("nome_cliente")
+    const data_cli = document.getElementById("data_cliente")
+    const valor_cod_pro = document.getElementById("codigo_produto")
+    const valor_desc_pro = document.getElementById("desc_produto")
+    const valor_preco_pro = document.getElementById("preco_produto")
+    const valor_quant_pro = document.getElementById("quantidade_produto")
+    const caixa_error = document.getElementById("quadrado-erro")
+    const fala_erro = document.getElementById("fala-erro")
+    const btn_ok = document.getElementById("btn_ok")
+    const lista_lateral = document.getElementById("lista_lateral")
+    const div_main = document.getElementById("div_main")
+
+    let i = 0
     //os X dos panels
     const x_cliente = document.getElementById("x-cliente")
     const x_produto = document.getElementById("x-produto")
     const x_pedido = document.getElementById("x-pedido")
-    sair.addEventListener("click", function(){
+    sair.addEventListener("click", function () {
         window.open("index.html", "_parent")
     })
     // passar pelas abas
-    clientes.addEventListener("click", function(){
+    clientes.addEventListener("click", function () {
         aba_cliente.classList.remove("desaparecer")
         aba_prod.classList.add("desaparecer")
         aba_ped.classList.add("desaparecer")
+        i = 0
+        abrir_clientes()
     })
-    x_cliente.addEventListener("click", function(){
+    x_cliente.addEventListener("click", function () {
         aba_cliente.classList.add("desaparecer")
     })
-    produto.addEventListener("click", function(){
-         aba_prod.classList.remove("desaparecer")
-         aba_cliente.classList.add("desaparecer")
-         aba_ped.classList.add("desaparecer")
-    })
-    x_produto.addEventListener("click", function(){
-        aba_prod.classList.add("desaparecer")
-    })
-    pedido.addEventListener("click", function(){
-        aba_ped.classList.remove("desaparecer")
-        aba_cliente.classList.add("desaparecer")
-        aba_prod.classList.add("desaparecer")
-   })
-   x_pedido.addEventListener("click", function(){
-    aba_ped.classList.add("desaparecer")
-})
-    // botoes <   >
-    botao_voltar.addEventListener("click", function(){
-        aba_ped.classList.remove("desaparecer")
-        aba_cliente.classList.add("desaparecer")
-    })
-    botao_avancar.addEventListener("click", function(){
-        aba_cliente.classList.add("desaparecer")
+    produto.addEventListener("click", function () {
         aba_prod.classList.remove("desaparecer")
+        aba_cliente.classList.add("desaparecer")
+        aba_ped.classList.add("desaparecer")
+        i = 0
+        abrir_produtos()
     })
-    botao_voltar_prod.addEventListener("click", function(){
-        aba_cliente.classList.remove("desaparecer")
+    x_produto.addEventListener("click", function () {
         aba_prod.classList.add("desaparecer")
     })
-    botao_avancar_prod.addEventListener("click", function(){
+    pedido.addEventListener("click", function () {
         aba_ped.classList.remove("desaparecer")
+        aba_cliente.classList.add("desaparecer")
         aba_prod.classList.add("desaparecer")
+    })
+    x_pedido.addEventListener("click", function () {
+        aba_ped.classList.add("desaparecer")
+    })
+    // botoes <   >
+        //clientes
+    botao_voltar.addEventListener("click", function () {
+        try{
+            i--
+            if (i == -1) throw new Error("Não há mais clientes na lista!")
+            abrir_clientes()
+            }catch(error){
+                causa_error(error)
+                i = 0
+            }
+    })
+    botao_avancar.addEventListener("click", function () {
+        try{
+            i++
+            if (i == dadosClientes.length) throw new Error("Não há mais clientes na lista!")
+            abrir_clientes()
+            }catch(error){
+                causa_error(error)
+                i = 0
+                abrir_clientes()               
+            }
+    })
+    // botoes <   >
+            //produtos
+    botao_voltar_prod.addEventListener("click", function () {
+        try{
+            i--
+            if (i == -1) throw new Error("Não há mais produtos na lista!")
+            abrir_produtos()
+            }catch(error){
+                causa_error(error)
+                i = 0
+            }
+    })
+    botao_avancar_prod.addEventListener("click", function () {
+        try{
+            i++
+            if (i == dadosProdutos.length) throw new Error("Não há mais produtos na lista!")
+            abrir_produtos()
+            }catch(error){
+                causa_error(error)
+                i = 0
+                abrir_produtos()               
+            }
+        })
+    //Pegar valor
+            //CLIENTES
+    function abrir_clientes() {
+        for (let valor in dadosClientes[i]) {
+            if (valor == "codCliente") {
+                valor_cod_cli.value = dadosClientes[i][valor]
+            }
+            if (valor == "nomeCliente") {
+                valor_nome_cli.value = dadosClientes[i][valor]
+            }
+            if (valor == "dataCadCli") {
+                data_cli.value = dadosClientes[i][valor]
+            }
+            console.log(dadosClientes[i][valor])
+        }
+    }
+    //PRODUTOS
+    function abrir_produtos() {
+        for (let valor in dadosProdutos[i]) {
+            if (valor == "codProduto") {
+                valor_cod_pro.value = dadosProdutos[i][valor]
+            }
+            if (valor == "descProduto") {
+                valor_desc_pro.value = dadosProdutos[i][valor]
+            }
+            if (valor == "precoProduto") {
+                valor_preco_pro.value = dadosProdutos[i][valor]
+            }
+            if (valor == "qtdEstoqueProd") {
+                valor_quant_pro.value = dadosProdutos[i][valor]
+            }
+        }
+    }
+    //mensagem error
+    function causa_error(error){
+        caixa_error.classList.remove("desaparecer")
+        div_main.classList.add("filtro_escuro")
+        lista_lateral.classList.add("filtro_escuro")
+        fala_erro.innerHTML = error.message
+    }
+    btn_ok.addEventListener("click", function(){
+        caixa_error.classList.add("desaparecer")
+        div_main.classList.remove("filtro_escuro")
+        lista_lateral.classList.remove("filtro_escuro")
     })
 }
